42591d11b431ff5d0af2b7103271e169014e6249
giddygitau <n.gideon.gitau@gmail.com>
2017-02-01 19:50:21 +0300

    [Chore #137592407] Adds list checks Asserts the checks of Alice 1 and Alice 2's timeout, grace, ping_url, status, last_ping, n_pings and pause_url Adds assertion for api key in request

--- hc/api/tests/test_list_checks.py
+++ hc/api/tests/test_list_checks.py
@@ -4,6 +4,7 @@ from django.utils.timezone import now
 
 from hc.api.models import Check
 from hc.test import BaseTestCase
+from hc.settings import PING_ENDPOINT, SITE_ROOT
 
 
 class ListChecksTestCase(BaseTestCase):
@@ -34,14 +35,44 @@ class ListChecksTestCase(BaseTestCase):
     def test_it_works(self):
         r = self.get()
         ### Assert the response status code
+        assert r.status_code == 200
 
         doc = r.json()
         self.assertTrue("checks" in doc)
 
         checks = {check["name"]: check for check in doc["checks"]}
         ### Assert the expected length of checks
+        assert len(checks) == 2
+
         ### Assert the checks Alice 1 and Alice 2's timeout, grace, ping_url, status,
         ### last_ping, n_pings and pause_url
+        # Timeout
+        response_a1 = checks['Alice 1']
+        response_a2 = checks['Alice 2']
+        a1 = {
+            'name': self.a1.name,
+            'timeout': int(self.a1.timeout.total_seconds()),
+            'grace': int(self.a1.grace.total_seconds()),
+            'ping_url': self.a1.url(),
+            'status': self.a1.status,
+            'last_ping': self.now.isoformat(),
+            'n_pings': self.a1.n_pings,
+            'pause_url': SITE_ROOT + '/api/v1/checks/' + str(self.a1.code) + '/pause'
+        }
+
+        a2 = {
+            'name': self.a2.name,
+            'timeout': int(self.a2.timeout.total_seconds()),
+            'grace': int(self.a2.grace.total_seconds()),
+            'ping_url': self.a2.url(),
+            'status': self.a2.status,
+            'last_ping': self.now.isoformat(),
+            'n_pings': self.a2.n_pings,
+            'pause_url': SITE_ROOT + '/api/v1/checks/' + str(self.a2.code) + '/pause'
+        }
+
+        self.assertDictContainsSubset(a1, response_a1)
+        self.assertDictContainsSubset(a2, response_a2)
 
     def test_it_shows_only_users_checks(self):
         bobs_check = Check(user=self.bob, name="Bob 1")
@@ -54,3 +85,8 @@ class ListChecksTestCase(BaseTestCase):
             self.assertNotEqual(check["name"], "Bob 1")
 
     ### Test that it accepts an api_key in the request
+    def test_it_accepts_api_key_in_request(self):
+        payload = {"api_key": "abc"}
+        r = self.client.post("/api/v1/checks/", json.dumps(payload), \
+                             content_type="application/json")
+        assert r.status_code == 201
