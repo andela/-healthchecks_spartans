e4b447c0d37b98b62aea439e9c1ed440368d209f
giddygitau <n.gideon.gitau@gmail.com>
2017-02-01 12:22:26 +0300

    [Chore #137592407] Adds test for test pause Adds assertion for methods not allowed

--- hc/api/tests/test_pause.py
+++ hc/api/tests/test_pause.py
@@ -13,6 +13,9 @@ class PauseTestCase(BaseTestCase):
                              HTTP_X_API_KEY="abc")
 
         ### Assert the expected status code and check's status
+        check.refresh_from_db()
+        assert r.status_code == 200
+        assert check.status == "paused"
 
     def test_it_validates_ownership(self):
         check = Check(user=self.bob, status="up")
@@ -25,3 +28,13 @@ class PauseTestCase(BaseTestCase):
         self.assertEqual(r.status_code, 400)
 
         ### Test that it only allows post requests
+    def test_it_only_allows_post_requests(self):
+        check = Check(user=self.alice, status="up")
+        check.save()
+
+        url = "/api/v1/checks/%s/pause" % check.code
+        r = self.client.get(url, "", content_type="application/json", \
+                             HTTP_X_API_KEY="abc")
+
+        # Get request should return status code 405 "method not allowed"
+        assert r.status_code == 405
