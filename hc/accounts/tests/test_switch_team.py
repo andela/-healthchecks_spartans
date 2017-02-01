c4c959cef34372c3f25751b864d328d69c44d4c8
Ivan Kipyegon <ivan@bardo.com>
2017-02-01 12:54:12 +0300

    [Chore #137592403] Add status code assertion in tests

--- hc/accounts/tests/test_switch_team.py
+++ hc/accounts/tests/test_switch_team.py
@@ -14,6 +14,7 @@ class SwitchTeamTestCase(BaseTestCase):
         r = self.client.get(url, follow=True)
 
         ### Assert the contents of r
+        self.assertEqual(r.status_code, 200)
 
 
     def test_it_checks_team_membership(self):
@@ -21,7 +22,7 @@ class SwitchTeamTestCase(BaseTestCase):
 
         url = "/accounts/switch_team/%s/" % self.alice.username
         r = self.client.get(url)
-        ### Assert the expected error code
+        ### Assert the expected error codec
 
     def test_it_switches_to_own_team(self):
         self.client.login(username="alice@example.org", password="password")
